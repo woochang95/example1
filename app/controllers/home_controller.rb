49bbac012594274175643a1dc989ca99e65c4836
Yang woo chang <dnckd7712@naver.com>
2017-05-08 00:15:52 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def write
+       @almond = params[:title]
+       @anchovy = params[:content]
+    end
+end
